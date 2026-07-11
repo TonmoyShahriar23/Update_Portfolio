f9658e964c02e1215cdaef72902dbbe9f4f304fc
TonmoyShahriar23 <tonmoy15-6052@diu.edu.bd>
2026-07-11 16:51:34 +0600

    first commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,10 @@
+﻿export default function Home() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center bg-[#FDFDFC] p-6 text-[#1b1b18]">
+            <h1 className="mb-2 text-3xl font-semibold">
+                🎉 Laravel + Inertia + React is working!
+            </h1>
+            <p className="text-[#706f6c]">Portfolio coming soon...</p>
+        </div>
+    );
+}
